8889e7368da367c70925482da1c659b83ce22927
Shackell <Nancy.Shackell@dfo-mpo.gc.ca>
2025-05-21 16:27:50 -0300

    GetPlotReady
    Gettingreadyto polot

---
+++ NancBranchDataScript/FancyFigs_ with_NF_PlotsbyNSh.R
@@ -0,0 +1,145 @@
+library(dplyr)
+
+library(broom)
+
+library(ggplot2)
+
+library(patchwork)
+
+library(grid)  # For unit() function
+#theme----
+theme_replace(panel.grid.minor = element_blank(), panel.grid.major = element_line(colour="black"),
+              
+              strip.background=element_rect(colour="black",fill="white"))
+
+theme_set(theme_bw())
+
+theme_replace(legend.key =element_rect(colour="black",fill="white"),
+              
+              plot.margin = unit(c(1.5,3,1.5,1), "cm"),
+              
+              #plot.margin=margin(3,4,4,0),
+              
+              #plot.margin=margin(3,4,4,0),
+              
+              plot.title=element_text(size=16,vjust=1.5,family="serif"),
+              
+              legend.background=element_rect(size=.9,colour="white",fill="white"),
+              
+              strip.text=element_text(size=14,family="serif",angle=0),
+              
+              panel.border = element_rect(colour = "black",fill=NA),
+              
+              panel.grid.minor = element_blank(), panel.grid.major = element_blank(),
+              
+              strip.background=element_rect(colour="black",fill="white"),
+              
+              axis.text.x = element_text(angle = 90, vjust = 0.2, hjust=1,size=12,family="serif"),
+              
+              axis.text.y = element_text(angle = 0, vjust = 0.5, hjust=1,size=12,family="serif"),
+              
+              axis.title.x=element_text(size=12,hjust=0.5,vjust=-2,family="serif"))
+
+pd <- position_dodge(.5)
+#END theme----
+# Plot abuncance indexed regions ----
+
+FigAbd.Region.Spring <- read.csv(here::here("2025-04-23/Output/IndexAbundance/abundance_ind_Region.Spring.csv"),row.names=NULL)
+names(FigAbd.Region.Spring)
+Reg_Abundance_coefficients_df <- read.csv(here::here("2025-04-23/Output/IndexAbundance/Reg_Abundance_coefficients_df.csv"),row.names=NULL)
+#To estimate the proportion ot toal abundance by Season, I have to do it here as teh script in 3.3Regional_Proportions can only easily be run if you have VAST
+proportions_and_density_Regional <- read.csv(here::here("2025-04-23/Output/Proportions/proportions_and_density_Regional.csv"),row.names=NULL)
+proportions_and_density_Regional$Period<-NULL
+proportions_and_density_Regional$Period[proportions_and_density_Regional$Year<2006]<-"Before Warming"
+proportions_and_density_Regional$Period[proportions_and_density_Regional$Year>2005]<-"During Warming"
+proportions_and_density_Regional.Spring<-subset(proportions_and_density_Regional, proportions_and_density_Regional$Season=="Spring")
+display.brewer.all(colorblindFriendly = TRUE)
+
+props <- proportions_and_density_Regional.Spring %>%
+  group_by(Period) %>%                               
+mutate(
+    Total_Estimate = sum(Index_Estimate),#total catch for the year
+    Proportion =  round(Index_Estimate / Total_Estimate, 3),  # Proportion per region rounded to 3 decimals
+    Proportion_SD = round(Index_SD / Total_Estimate, 3) #the SD also needs to be scaled by the same factor in order to remain consistent
+  ) 
+  
+##Figure ABD Panel A----
+regpal<- c("orange", "darkblue")
+colbline<-brewer.pal(3, "Dark2")
+custom_labels <- c(
+  "Canada: Before (96%), During (94%) Change (2%)",
+  "USA:    Before ( 4%), During ( 6%) Change (35%)"
+)
+ARegionalPlot<- 
+  ggplot(data = FigAbd.Region.Spring, aes(x = Year, y = Index_Estimate/1000000),group=Region)+
+  geom_vline(xintercept=2005,lty=2,lwd=1.2)+
+  #geom_errorbar(data =  FigAbd.Region.Spring, aes(x = Year, ymin = (Index_Estimate/1000000 - Index_SD/1000000), ymax = (Index_Estimate/1000000 + Index_SD/1000000), color = Region, group = Region), alpha = 0.65) +
+  geom_ribbon(aes(x = Year, ymin = Index_Estimate/1000000 - Index_SD/1000000, 
+                  ymax = Index_Estimate/1000000 +Index_SD/1000000, 
+                  fill = Region), alpha = 0.12) +  # Use geom_ribbon for the SE band
+  geom_line(aes(color =  Region), linewidth = 1) +                         # Line for the group
+  geom_point(aes(color = Region), shape = 19,size=2.5) +                        # Points for data
+  scale_fill_manual(values = regpal) +                    # Custom fill colors
+  scale_color_manual(values = regpal) +
+  # Combine legends
+  guides(color = guide_legend(title = ""),
+         fill = "none") + 
+  labs(y="Modelled Abundance (Millions)", x="")+
+  guides(color = guide_legend(title = ""))+
+  annotate("text", x = 1993, y = 7, label = "Before Warming", color = "black", size = 5,family = "serif") +
+  annotate("text", x = 2012, y = 7, label = "During Warming", color = "black", size = 5,family = "serif") +
+  #Before
+  annotate("text", x = 1995, y = 3.1, label = "96%", color = "black", size = 4) +
+  annotate("segment", x = 1995, xend = 1995, y = 2.7, yend = 2, arrow = arrow(length = unit(.2, "cm"),type="closed")) +
+  annotate("text", x = 1992, y = .85, label = "4%", color = "black", size = 4) +
+  annotate("segment", x = 1992, xend = 1992, y = .7, yend = .3, arrow = arrow(length = unit(.2, "cm"),type="closed")) +
+  #After
+  annotate("text", x = 2015, y = 6.2, label = "94%", color = "black", size = 4) +
+  annotate("segment", x = 2015, xend = 2015, y = 5.7, yend = 5.2, arrow = arrow(length = unit(.2, "cm"),type="closed")) +
+  annotate("text", x = 2015, y = .9, label = "6%", color = "black", size = 4) +
+  annotate("segment", x = 2015, xend = 2015, y = .7, yend = .3, arrow = arrow(length = unit(.2, "cm"),type="closed")) +
+  #annotate("rect", xmin = 2004, xmax = 2014, ymin = 3, ymax = 5, alpha = 0.2, fill = "red") +
+  theme(text = element_text(family = "serif"),  
+        legend.box.background = element_blank(), # Transparent legend box
+        legend.position = c(.15,.7),
+        legend.text = element_text(size = 14,family="serif"),
+        plot.margin=margin(10, 5, 10, 15))
+ARegionalPlot
+#END Figure ABD A----
+#Figure ABD RATE----
+FigAbd.RegionCoef$Ord2Region<-factor(FigAbd.RegionCoef$Region, levels=c("USA","Canada"))
+#FigAbd.RegionCoef$RevPeriod <- factor(FigAbd.RegionCoef$Period, levels = c("During Warming", "Before Warming")) 
+RegionRatesPlot<-
+  ggplot(FigAbd.RegionCoef, aes(x = factor(Ord2Region), y = estimate,fill=Period)) +
+  geom_errorbar(aes(ymin = conf.low, ymax = conf.high), position = pd) +
+  geom_point(shape = 21, size = 3, position = pd) +
+  coord_flip() +
+  guides(fill = guide_legend(reverse = TRUE)) +  # Reverse the legend order
+  scale_fill_manual(values = c("steelblue","orangered" ))+ #Reverse the colors
+  geom_hline(yintercept = 0, linetype = "dashed") + # Dashed line for y=0
+  ylim(-0.06, 0.06) +
+  #theme_minimal() + # A cleaner minimal theme
+  theme(
+    text = element_text(family = "serif"),  
+    legend.position = c(0.25, 0.6),               # Position of the legend
+    legend.box.background = element_blank(), # Transparent legend box
+    legend.title = element_blank(),             # Hide legend title
+    legend.text = element_text(size = 12, family = "serif"), # Customize legend text
+    axis.text = element_text(size = 14),      # Customize x-axis label
+    axis.title.y = element_blank(),             # Remove y-axis label
+    axis.title.x = element_text(size = 14),      # Customize x-axis label
+    plot.margin=margin(10,40,20,30))+
+  xlab("") +                                     # Clear x-axis label
+  ylab("Rate of change in Abundance (log10) /year")
+RegionRatesPlot
+Figure2AbdAbdRates<-plot_grid(ARegionalPlot, RegionRatesPlot, nrow = 2,rel_heights = c(2, 1),labels = c("(a)", "(b)"))#,align = "v", axis = "lr") # Add labels
+Figure2AbdAbdRates
+#ggsave(here::here("R/DataforFinalFigs/Figure2AbdAbdRates.tiff"), plot = Figure2AbdAbdRates, dpi = 600, width = 8, height = 6, units = "in", device = "tiff")
+# END Plot abundance indexed regions  ----
+ggsave(here::here("R/DataforFinalFigs/Figure2AbdAbdRates.jpeg"), plot = Figure2AbdAbdRates, dpi = 600, width = 8, height = 6, units = "in", device = "jpeg")
+#EOA plots and table for supplemental----
+# PLOT EAO,----
+
+
+
+
