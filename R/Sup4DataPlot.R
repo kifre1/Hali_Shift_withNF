7fc5ba2b4bbe355099f51011cf05f8faab6ff466
kifre1 <kiyomi.french@gmail.com>
2025-07-18 17:18:19 -0300

    Added part 3 (S2 Fig2, panel plot of survey locations by year)

--- R/Sup4DataPlot.R
+++ R/Sup4DataPlot.R
@@ -1,6 +1,6 @@
 #Part1: pull in ALL NEFSC, DFO Maritimes and NF data to plot the distribution of tows and presence
 #Part2: Make Appendix Figure 1: Footprint of NEFSC and DFO Maritimes, and NF  RV surveys 
-
+#Part3: Make Appendix Figure 2: Panel plot showing the data availability by Year
 
 library(tidyverse)
 library(sf)
@@ -148,6 +148,9 @@ summary(catch_df)
 library(sf)
 library (ggplot2)
 
+tows_df<-subset(tows_df, tows_df$year > 1989)
+tows_df<-subset(tows_df, tows_df$year < 2024)
+
 #see the extent
 extent <- tows_df |>
   distinct(longitude, latitude) |>
@@ -160,7 +163,7 @@ All_region <- st_read(here::here("", "R/Shapefiles/IndexShapefiles/Full_RegionAl
 crs <- st_crs(All_region)
 
 #bounding box for plotting
-bbox <- st_bbox(c(xmin = -78, ymin = 33, xmax = -40, ymax = 73))
+bbox <- st_bbox(c(xmin = -78, ymin = 33, xmax = -40, ymax = 70))
 bbox_polygon <- st_as_sfc(bbox)
 st_crs(bbox_polygon) <- crs
 
@@ -202,7 +205,7 @@ All_Trawls_Plot<-ggplot() +
   geom_sf(data = land, fill="cornsilk") +
   geom_point (data = tows_df, aes(x= longitude , y = latitude, col= survey),  alpha = .6, size = .2, shape=19)+
   scale_color_manual(values = c("NEFSC" = "steelblue", "DFO" =  "orangered", "NF" = "darkred"))+ 
-  xlim(-77, -42) + ylim(35, 70)+
+  xlim(-76, -42) + ylim(35, 67)+
   theme_bw()+
   labs(title=NULL, 
        x = NULL, 
@@ -232,7 +235,7 @@ All_catch_plot<-ggplot() +
   geom_sf(data = land, fill="cornsilk") +
   geom_point (data = presence_only, aes(x= longitude , y = latitude, col= survey),  alpha = .6, size = .2, shape=19)+
   scale_color_manual(values = c("NEFSC" = "steelblue", "DFO" =  "orangered", "NF" = "darkred"))+ 
-  xlim(-77, -42) + ylim(35, 70)+
+  xlim(-76, -42) + ylim(35, 67)+
   theme_bw()+
   labs(title=NULL, 
        x = NULL, 
@@ -255,19 +258,127 @@ All_catch_plot<-ggplot() +
 SurveyPlot<-grid.arrange(All_Trawls_Plot,All_catch_plot, padding = unit(0, "line"))
 #add labels 
 SurveyPlot_final <- ggdraw(SurveyPlot) +
-  draw_plot_label(label = "(a)", x = 0.12, y = 0.998, size = 12) + 
-  draw_plot_label(label = "(b)", x = 0.385, y = 0.998, size = 12) +   
-  draw_plot_label(label = "(c)", x = 0.657, y = 0.998, size = 12) + 
-  draw_plot_label(label = "(d)", x = 0.12, y = 0.5, size = 12) +  
-  draw_plot_label(label = "(e)", x = 0.385, y = 0.5, size = 12) +  
-  draw_plot_label(label = "(f)", x = 0.657, y = 0.5, size = 12)    
+  draw_plot_label(label = "(a)", x = 0.08, y = 0.998, size = 12) + 
+  draw_plot_label(label = "(b)", x = 0.37, y = 0.998, size = 12) +   
+  draw_plot_label(label = "(c)", x = 0.665, y = 0.998, size = 12) + 
+  draw_plot_label(label = "(d)", x = 0.08, y = 0.5, size = 12) +  
+  draw_plot_label(label = "(e)", x = 0.37, y = 0.5, size = 12) +  
+  draw_plot_label(label = "(f)", x = 0.665, y = 0.5, size = 12)    
 
 #SAVE plots SUPPLEMTAL FIGURE 1
 ggsave(
-  filename = "SurveyLocPlot.png",
+  filename = "SurveyLocsPlot.png",
   plot = SurveyPlot_final,          # optional if it's the last plot
   path =  here::here("2025-04-23/FinalPlots"),
   width = 6.5,
   height = 7.5,
+  dpi = 300
+)
+
+#PART 3
+#panel plot showing the data availability by facet(.~year) 
+#season by different viridis colors, 
+# different symbols surveys
+head(tows_df)
+
+#fill in if missing season, remove winter
+tows_df_2 <- tows_df %>%
+  mutate(
+    season = case_when(
+      is.na(season) & month(date) %in% c(3, 4, 5)  ~ "Spring",
+      is.na(season) & month(date) %in% c(6, 7, 8)  ~ "Summer",
+      is.na(season) & month(date) %in% c(9, 10, 11) ~ "Fall",
+      TRUE ~ season  # Keep existing SEASON values
+    )
+  )
+tows_df_2<-subset(tows_df_2, tows_df_2$season !="Winter")
+
+#checks
+range(tows_df_2$year)
+unique(tows_df_2$season)
+unique(tows_df_2$survey)
+range(tows_df_2$longitude)
+range(tows_df_2$latitude)
+sum(is.na(tows_df_2$longitude))
+sum(is.na(tows_df_2$latitude))
+sum(is.na(tows_df_2$season))
+sum(is.na(tows_df_2$survey))
+
+summary_table <- tows_df_2 %>%
+  group_by(survey, season) %>%
+  summarise(count = n()) %>%
+  arrange(survey, season)
+
+#load and clip the shapefiels for plotting 
+#bounding box for plotting
+bbox <- st_bbox(c(xmin = -77, ymin = 32, xmax = -42, ymax = 68))
+bbox_polygon <- st_as_sfc(bbox)
+st_crs(bbox_polygon) <- crs
+
+#Mapping shapefiles
+EEZ <- st_read(here::here("", "Data/Mapping_shapefiles/EEZ.shp"))
+land <- st_read(here::here("", "Data/land_shapefile/ne_50m_land.shp"))
+contours <- st_read(here::here("", "Data/Mapping_shapefiles/GEBCO_DepthContours.shp"))
+NAFO <- st_read(here::here("", "Data/Mapping_shapefiles/Divisions.shp"))
+Hague <- st_read(here::here("", "Data/Mapping_shapefiles/HagueLine.shp"))
+
+EEZ <- st_transform (EEZ, crs)
+land <- st_transform (land, crs)
+contours <- st_transform (contours, crs)
+NAFO <- st_transform (NAFO, crs)
+Hague <- st_transform (Hague, crs)
+sf::sf_use_s2(FALSE)   # revert to GEOS-based operations
+
+# Clip large shapefiles shapefile to  bounding box
+EEZ <- st_intersection(EEZ, bbox_polygon)
+land <- st_intersection(land, bbox_polygon)#maybe comment out 
+contours <- st_intersection(contours, bbox_polygon)
+NAFO <- st_intersection(NAFO, bbox_polygon)
+
+
+#plot
+Annual_Pannels<-ggplot() +
+  geom_sf(data = NAFO, color="darkblue", fill = NA) +
+  geom_sf(data = Hague, color="black", size = 3) +
+  geom_sf(data = EEZ, color="black", linetype = "dashed", size = 3) +
+  geom_sf(data = land, fill="cornsilk") +
+  geom_point (data = tows_df_2, 
+              aes(x= longitude , y = latitude, col= season), #took out shape = survey becasue it is too small to see
+              alpha = .6, size = .2)+
+  scale_color_manual(
+    values = c("Spring" = "#440154", "Summer" =  "#21908C", "Fall" = "#B5DE2B"))+
+ # scale_shape_manual(
+ #   values = c("NEFSC" = 16, "DFO" = 17, "NF" = 15))+
+  xlim(-76, -43) + ylim(35, 67)+
+  theme_bw()+
+  labs(title=NULL, 
+       x = NULL, 
+       y = NULL, 
+       fill = "season",
+       shape = "survey")+
+  guides(
+    color = guide_legend(override.aes = list(size = 5, alpha = 1)),  # for color legend
+    shape = guide_legend(override.aes = list(size = 5))   # for shape legend, if applicable
+  )+
+  theme(
+        legend.background=element_rect(size=.9,colour="white",fill="white"),
+        strip.text=element_text(size=8,family="serif",angle=0, margin = margin(t = 1, b = 1)),
+        panel.border = element_rect(colour = "black",fill=NA),
+        panel.grid.minor = element_blank(), panel.grid.major = element_blank(),
+        strip.background=element_rect(fill="white",size = 0.5,colour="black"),
+        axis.text.x = element_text(angle = 90, vjust = 1, hjust=1,size=9,family="serif"),
+        axis.text.y = element_text(angle = 0, vjust = 0.5, hjust=1,size=9,family="serif"),
+        panel.spacing = unit(0, "lines"),
+        legend.position = c(0.94, 0.06),
+        plot.margin=margin(3,3,3,3))+
+  facet_wrap(~ year, ncol = 7, nrow = 5)
+Annual_Pannels
+
+ggsave(
+  filename = "AnnualSurveyLocs.png",
+  plot = Annual_Pannels,          # optional if it's the last plot
+  path =  here::here("2025-04-23/FinalPlots"),
+  width = 6.5,
+  height = 8,
   dpi = 300
 )
